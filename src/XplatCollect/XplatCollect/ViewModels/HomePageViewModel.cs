d62e25c1f6e1e353c883dda25f38c77f805737df
GabrielDragone <37886654+GabrielDragone@users.noreply.github.com>
2019-09-14 10:41:53 -0300

    :construction_worker: instalação viewmodel

--- src/XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
+++ src/XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
@@ -1,18 +1,28 @@
 ﻿using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace XplatCollect.ViewModels
 {
-    public sealed class HomePageViewModel : BindableBase
+    public sealed class HomePageViewModel : ViewModelBase
     {
-        //Atributo:
+
         private string title;
+
+        public HomePageViewModel(INavigationService navigationService
+            , IPageDialogService pageDialogService)
+            : base(navigationService, pageDialogService)
+        {
+            Title = "Collect App";
+        }
+
         public string Title
         {
             get => title;
-            set => SetProperty(ref title, value); //Framework
+            set => SetProperty(ref title, value);
         }
     }
 }
